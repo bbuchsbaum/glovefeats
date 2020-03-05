b76f0ba497cf0ad7fe291e04fdfe43c80ec0c74b
Bradley Buchsbaum <bbuchsbaum@Umbrellabird.local>
2020-03-05 07:18:09 -0500

    feats

---
+++ R/features.R
@@ -0,0 +1,20 @@
+
+glove_features <- function(words) {
+  if (!exists("glove50")) {
+    data("glove50")
+  }
+
+  glove50[words]
+
+}
+
+word_sim <- function(words, method="cosine") {
+  feats <- glove_features(words)
+  fmat <- do.call(rbind, feats$vec)
+  ret <- proxy::simil(fmat, method)
+  ret <- as.matrix(ret)
+  colnames(ret) <- words
+  rownames(ret) <- words
+  ret
+}
+
