212c4d8c8990bc37e4f66c8079156d88dc3a2101
Bradley Buchsbaum <bbuchsbaum@Umbrellabird.local>
2020-05-18 17:30:21 -0400

    handle NAs

--- R/features.R
+++ R/features.R
@@ -29,7 +29,11 @@ wordpair_sim <- function(words, method="cosine") {
   for (i in 1:nrow(words)) {
     f1 <- glove_features(words[i,1])
     f2 <- glove_features(words[i,2])
-    sim[i] <- proxy::simil(rbind(f1$vec[[1]],f2$vec[[1]]), method)
+    if (is.null(f1$vec[[1]]) || is.null(f2$vec[[1]])) {
+      sim[i] <- NA
+    } else {
+      sim[i] <- proxy::simil(rbind(f1$vec[[1]],f2$vec[[1]]), method)
+    }
   }
 
   sim
