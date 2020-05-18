af53cea3dbf3ac5f4e641391e2e4f36b27ed897b
Bradley Buchsbaum <bbuchsbaum@Umbrellabird.local>
2020-05-18 17:18:19 -0400

    added wordpair_sim

--- R/features.R
+++ R/features.R
@@ -21,3 +21,17 @@ word_sim <- function(words, method="cosine") {
   ret
 }
 
+#' @export
+#' @import assertthat
+wordpair_sim <- function(words, method="cosine") {
+  assertthat::assert_that(ncol(words) == 2)
+  sim <- numeric(nrow(words))
+  for (i in 1:nrow(words)) {
+    f1 <- glove_features(words[i,1])
+    f2 <- glove_features(words[i,2])
+    sim[i] <- proxy::simil(rbind(f1$vec[[1]],f2$vec[[1]]), method)[1,1]
+  }
+
+  sim
+}
+
